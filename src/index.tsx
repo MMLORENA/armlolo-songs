6bb26a3f658597159b990ef6993a3530d771d7b7
Lorena Martinez Moledo <lmartmol@gmail.com>
2022-11-12 12:34:03 +0100

    Create globalStyle with general resets & delete unnecessary index.css

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "./GlobalStyle";
+import App from "./components/App/App";
+import mainTheme from "./mainTheme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <GlobalStyle />
+    <ThemeProvider theme={mainTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
