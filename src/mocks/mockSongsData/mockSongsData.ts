8e9cb2daaa760fffa18f756bb2f64bce1155c32e
David Lozano <dawgun@hotmail.com>
2022-11-12 11:52:29 +0100

    Test loadSongsActionCreator

---
+++ src/mocks/mockSongsData/mockSongsData.ts
@@ -0,0 +1,9 @@
+import { Song } from "../../store/contexts/types";
+
+export const mockListSongs: Song[] = [
+  {
+    artist: "Andy&Lucas",
+    picture: "andy_lucas.jpg",
+    title: "Y yo la quería",
+  },
+];
