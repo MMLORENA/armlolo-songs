355fce98ac1462d2a1cb3cd5914ffb7fc4f26d70
Lorena Martinez Moledo <lmartmol@gmail.com>
2022-11-12 16:04:33 +0100

    Test Header Component

---
+++ src/components/Header/Header.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/react";
+import { ThemeProvider } from "styled-components";
+import Header from "./Header";
+import mainTheme from "../../mainTheme";
+
+describe("Given the Header Component", () => {
+  describe("When it's render", () => {
+    test("Then should show a 'Music Addicts' in a heading level 1", () => {
+      const expectedTitle = "Music Addicts";
+      render(
+        <ThemeProvider theme={mainTheme}>
+          <Header />
+        </ThemeProvider>
+      );
+
+      const resultTitle = screen.getByRole("heading", { name: expectedTitle });
+
+      expect(resultTitle).toBeInTheDocument();
+    });
+  });
+});
