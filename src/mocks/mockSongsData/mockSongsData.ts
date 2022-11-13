716b451304bd8f53b6272e96c8a5a0efdb6d3422
Lorena Martinez Moledo <lmartmol@gmail.com>
2022-11-13 13:37:32 +0100

    Add to type Song more metaData properties

--- src/mocks/mockSongsData/mockSongsData.ts
+++ src/mocks/mockSongsData/mockSongsData.ts
@@ -3,5 +3,9 @@ import { Song } from "../../store/contexts/types";
 export const mockSong: Song = {
   artist: "Armthis&Lojelzy",
   picture: "arm_loj.jpg",
-  title: "Lo que diga Lorena",
+  title: "Bad Jokes",
+  id: "1",
+  album: "Bad Jokes",
+  time: "1d",
+  audio: "./",
 };
