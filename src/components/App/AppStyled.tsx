30ada4db7716711110665d70b015243150e7ecf2
David Lozano <dawgun@hotmail.com>
2022-11-13 14:39:26 +0100

    Add routes in App for home and root path

--- src/components/App/AppStyled.tsx
+++ src/components/App/AppStyled.tsx
@@ -2,12 +2,14 @@ import styled from "styled-components";
 
 const AppStyled = styled.div`
   display: flex;
-  justify-content: center;
   padding: 0.5rem;
   background-color: ${(props) => props.theme.mainDark};
   color: ${(props) => props.theme.mainLight};
   min-height: 100vh;
   width: 100%;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
 
   .a {
     display: flex;
