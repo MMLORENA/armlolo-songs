3908296b9798daa2589cb00bb843ca192c44c7e4
David Lozano <dawgun@hotmail.com>
2022-11-13 12:47:59 +0100

    Create factory songs with fishery and faker libraries for tests

--- src/mocks/mockSongsData/mockSongsData.ts
+++ src/mocks/mockSongsData/mockSongsData.ts
@@ -1,7 +1,18 @@
+import { faker } from "@faker-js/faker";
+import { Factory } from "fishery";
 import { Song } from "../../store/contexts/types";
 
-export const mockSong: Song = {
-  artist: "Armthis&Lojelzy",
-  picture: "arm_loj.jpg",
-  title: "Lo que diga Lorena",
-};
+const numberOfSongs = 2;
+
+const songFactory = Factory.define<Song>(() => ({
+  artist: faker.internet.userName(),
+  picture: faker.image.imageUrl(),
+  title: faker.music.songName(),
+  album: faker.company.name(),
+  audio: faker.system.filePath(),
+  id: faker.datatype.uuid(),
+  time: `${faker.datatype.datetime()}`,
+}));
+
+export const mockSong = songFactory.build();
+export const mockListSong = songFactory.buildList(numberOfSongs);
