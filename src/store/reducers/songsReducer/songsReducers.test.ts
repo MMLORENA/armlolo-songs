0c00e6ae7ca463bedc129b60c8283023554de536
David Lozano <dawgun@hotmail.com>
2022-11-13 13:00:18 +0100

    Add test for addSong case of songsReducer

--- src/store/reducers/songsReducer/songsReducers.test.ts
+++ src/store/reducers/songsReducer/songsReducers.test.ts
@@ -1,16 +1,23 @@
+import {
+  mockListSong,
+  mockSong,
+} from "../../../mocks/mockSongsData/mockSongsData";
 import { mockStructureSongsData } from "../../../mocks/mockStructureSongsData/mockStrutureSongsData";
-import { loadSongsActionCreator } from "../../actions/actionsSongs/actionsCreatorSongs";
+import {
+  addSongActionCreator,
+  loadSongsActionCreator,
+} from "../../actions/actionsSongs/actionsCreatorSongs";
 import { SongAction } from "../../actions/actionsSongs/types/actionsSongs";
 import { SongsStructure } from "../../contexts/types";
 import songsReducer from "./songsReducer";
 
 describe("Given the songsReducers function", () => {
   describe("When it's called with a previous state and an action with type 'loadSongs' and a list of songs in payload", () => {
-    test("Then should return a new state with a new list of songs", () => {
-      const loadSongsAction = loadSongsActionCreator([]);
+    test("Then it should return a new state with a new list of songs", () => {
+      const loadSongsAction = loadSongsActionCreator(mockListSong);
       const expectedNewState: SongsStructure = {
-        ...mockStructureSongsData,
-        songs: [],
+        songActive: mockSong,
+        songs: mockListSong,
       };
 
       const newState = songsReducer(mockStructureSongsData, loadSongsAction);
@@ -19,8 +26,22 @@ describe("Given the songsReducers function", () => {
     });
   });
 
+  describe("When it's called with a previous state and and action with type 'addSongs' and a song", () => {
+    test("Then it should return a new state same previous state with the new song received", () => {
+      const addSongAction = addSongActionCreator(mockSong);
+      const expectedNewState: SongsStructure = {
+        songActive: mockSong,
+        songs: [...mockListSong, mockSong],
+      };
+
+      const newState = songsReducer(mockStructureSongsData, addSongAction);
+
+      expect(newState).toStrictEqual(expectedNewState);
+    });
+  });
+
   describe("When it's called with a previous state and an action with unknown type", () => {
-    test("Then should return a new state same the previous state", () => {
+    test("Then it should return a new state same the previous state", () => {
       const unknownAction: SongAction = { type: "unknown" };
       const expectedNewState = { ...mockStructureSongsData };
 
