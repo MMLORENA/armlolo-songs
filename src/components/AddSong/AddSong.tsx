ee558ab3ed11463f332946f8e994bbe6567bcf6c
Adrian Armesto <armestomena@gmail.com>
2022-11-13 13:01:52 +0100

    Add a use satete to get the name of the file

--- src/components/AddSong/AddSong.tsx
+++ src/components/AddSong/AddSong.tsx
@@ -1,12 +1,26 @@
+import React, { useState } from "react";
 import AddSongStyled from "./AddSongStyled";
 
 const AddSong = (): JSX.Element => {
+  const noAudioSelected = "";
+  const [audioName, setAudioName] = useState(noAudioSelected);
+
+  const changeAudioName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    let fileName = event.target.files![0].name;
+    setAudioName(fileName);
+  };
   return (
     <AddSongStyled className="add-audio">
       <label className="add-audio__input-label">
         Click here to provide a song
-        <input className="add-audio__input" type="file" accept=".mp3" />
+        <input
+          className="add-audio__input"
+          onChange={(event) => changeAudioName(event)}
+          type="file"
+          accept=".mp3"
+        />
       </label>
+      <span className="add-audio__selected-file">{audioName}</span>
       <input className="add-audio__submit" type="submit" value="Send song" />
     </AddSongStyled>
   );
