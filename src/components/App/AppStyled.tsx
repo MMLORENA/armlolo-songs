f7111d350ae8af64fe9ba0075078b13f39820476
Lorena Martinez Moledo <lmartmol@gmail.com>
2022-11-13 18:25:10 +0100

    Modify padding layaout & change margin 0 auto to body

--- src/components/App/AppStyled.tsx
+++ src/components/App/AppStyled.tsx
@@ -4,7 +4,7 @@ const AppStyled = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 0.5rem 2rem;
+  padding: 0.5rem;
   background-color: ${(props) => props.theme.mainDark};
   color: ${(props) => props.theme.mainLight};
   min-height: 100vh;
