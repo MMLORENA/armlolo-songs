379b856b8231e9ac3d286b9a5dbaf2a2337f23f7
Adrian Armesto <armestomena@gmail.com>
2022-11-16 17:25:56 +0100

    Delete song player on home page to add it in another branch

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,27 +1,13 @@
-import { useContext } from "react";
 import AddSong from "../../components/AddSong/AddSong";
-import SongPlayer from "../../components/SongPlayer/SongPlayer";
 import SongsList from "../../components/SongsList/SongsList";
-import SongsContext from "../../store/contexts/SongsContext/SongsContext";
 import HomepageStyled from "./HomepageStyled";
 
 const Homepage = (): JSX.Element => {
-  const {
-    songActive: { title, album, audio, picture, artist },
-  } = useContext(SongsContext);
-
   return (
     <HomepageStyled className="homepage-container">
       <h2 className="homepage-title">Homepage</h2>
       <AddSong />
       <SongsList />
-      <SongPlayer
-        title={title ? title : "Unknown"}
-        album={album ? album : "Unknown"}
-        artist={artist ? artist : "Unknown"}
-        audio={audio ? audio : ""}
-        picture={picture ? picture : "unknown.png"}
-      />
     </HomepageStyled>
   );
 };
