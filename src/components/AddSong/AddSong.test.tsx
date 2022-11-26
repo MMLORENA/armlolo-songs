99c4c3010e36433104842b59008c0abdc487b73f
Adrian Armesto <armestomena@gmail.com>
2022-11-26 17:23:44 +0100

    Test on submit add song component

--- src/components/AddSong/AddSong.test.tsx
+++ src/components/AddSong/AddSong.test.tsx
@@ -4,6 +4,12 @@ import { WrapperProps } from "../../testUtils/wrappers/types/types";
 import WrapperRender from "../../testUtils/wrappers/WrapperRender";
 import AddSong from "./AddSong";
 
+const mockAddSong = jest.fn();
+
+jest.mock("../../hooks/useSong", () => () => ({
+  addSong: mockAddSong,
+}));
+
 describe("Given the Add Song component", () => {
   describe("When it's render", () => {
     const componentWithOptions: WrapperProps = {
@@ -32,6 +38,27 @@ describe("Given the Add Song component", () => {
       const songNameDocument = screen.queryByText(songName);
 
       expect(songNameDocument).toBeInTheDocument();
+    });
+
+    describe("And the user send the song after select it", () => {
+      test("Then it should call the add song function with the uploaded song", () => {
+        WrapperRender(componentWithOptions);
+
+        const audioInput = screen.getByLabelText(
+          "Click here to provide a song"
+        );
+
+        const songName = "async.mp3";
+        const fakeSong = new File([], songName);
+
+        userEvent.upload(audioInput, fakeSong);
+
+        const submitButton = screen.getByRole("button", { name: "Send Song" });
+
+        userEvent.click(submitButton);
+
+        expect(mockAddSong).toHaveBeenCalledWith(fakeSong);
+      });
     });
   });
 });
