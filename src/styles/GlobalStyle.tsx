2fab34f2f23ed81f25cbb7b1f99d6322ac94e23a
Lorena Martinez Moledo <lmartmol@gmail.com>
2022-11-26 20:00:23 +0100

    Custome toastify colors & add Toasty Container to App

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -9,6 +9,38 @@ html {
     font-family: "Lexend Deca", sans-serif;
   }
 
+:root {
+  --toastify-color-light: ${(props) => props.theme.colors.mainLight};
+  --toastify-color-dark:${(props) => props.theme.colors.mainDark};
+
+  --toastify-icon-color-info: ${(props) => props.theme.colors.secondaryColor};
+  --toastify-icon-color-success: ${(props) =>
+    props.theme.colors.secondaryColor};
+  --toastify-icon-color-warning: var(--toastify-color-warning);
+  --toastify-icon-color-error: ${(props) => props.theme.colors.primaryColor};
+
+  --toastify-toast-width: 320px;
+  --toastify-toast-background: ${(props) => props.theme.colors.mainLight};
+  --toastify-toast-min-height: 64px;
+  --toastify-toast-max-height: 800px;
+  --toastify-font-family: inherit;
+  --toastify-z-index: 9;
+
+  --toastify-text-color-light: ${(props) => props.theme.colors.secondaryColor};
+  --toastify-text-color-dark: ${(props) => props.theme.colors.primaryColor};
+
+  --toastify-color-progress-light:${(props) =>
+    props.theme.colors.secondaryColor}; 
+  --toastify-color-progress-dark:${(props) =>
+    props.theme.colors.secondaryColor};
+  --toastify-color-progress-info: ${(props) =>
+    props.theme.colors.secondaryColor};
+  --toastify-color-progress-success: ${(props) =>
+    props.theme.colors.secondaryColor};
+  --toastify-color-progress-error: ${(props) =>
+    props.theme.colors.primaryColor};
+}
+
 body {
   margin: 0 auto;
 }
