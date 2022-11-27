e288c49bbb0000011cdaa1b2a14242bc15353ebc
Adrian Armesto <armestomena@gmail.com>
2022-11-27 12:33:13 +0100

    Fix a bug on songs list to take all the width in computer

--- src/components/SongsList/SongsListStyled.tsx
+++ src/components/SongsList/SongsListStyled.tsx
@@ -3,12 +3,15 @@ import styled from "styled-components";
 const SongsListStyled = styled.main`
   width: 100%;
   display: flex;
+  flex-direction: column;
+  align-items: center;
   justify-content: center;
 
   .songs-container {
     display: flex;
     flex-direction: column;
     gap: 1rem;
+    width: 100%;
   }
 
   .none-songs {
