ec6a228a0f21812c1b6b535ea47adc8e801b0c93
Lorena Martinez Moledo <lmartmol@gmail.com>
2022-11-27 12:59:42 +0100

    Change Button component props & add icon style

--- src/components/Button/ButtonStyled.ts
+++ src/components/Button/ButtonStyled.ts
@@ -1,14 +1,51 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-const ButtonStyled = styled.button`
+interface ButtonStyledProps {
+  semantic: "button" | "icon";
+}
+
+const buttonStyle = css`
   background-color: ${({ theme: { colors } }) => colors.primaryColor};
   border-radius: 5px;
   border: none;
   padding: 0.5rem;
+  min-width: 5rem;
+  min-height: 2rem;
+  width: fit-content;
+  height: fit-content;
 
   &:hover {
     background-color: ${({ theme: { colors } }) => colors.secondaryColor};
   }
+
+  &:disabled {
+    background-color: ${({ theme: { colors } }) => colors.mainLight};
+    border: ${({ theme: { colors } }) => colors.secondaryColor} 2px solid;
+  }
+`;
+
+const iconStyle = css`
+  border-radius: 50px;
+  border: ${({ theme: { colors } }) => colors.primaryColor} 2px solid;
+  width: 30px;
+  height: 30px;
+  background-color: ${({ theme: { colors } }) => colors.mainLight};
+  color: ${({ theme: { colors } }) => colors.secondaryColor};
+
+  &::before {
+    content: "X";
+    text-align: center;
+  }
+
+  &:hover {
+    color: ${({ theme: { colors } }) => colors.primaryColor};
+    border: ${({ theme: { colors } }) => colors.secondaryColor} 2px solid;
+  }
+`;
+
+const ButtonStyled = styled.button<ButtonStyledProps>`
+  ${(props) => props.semantic === "button" && buttonStyle};
+  ${(props) => props.semantic === "icon" && iconStyle}
 `;
 
 export default ButtonStyled;
